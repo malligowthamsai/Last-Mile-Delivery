6e9a965070dc4ee079654d81416bf294f83a2a81
Gowtham <malligowthamsai@gmail.com>
2026-08-24 20:26:06 +0530

    Delete backend-express directory

--- backend-express/src/lib/prisma.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-
-// Singleton pattern to reuse Prisma client across modules
-const prisma = new PrismaClient({
-  log: process.env.NODE_ENV === 'development' ? ['query', 'error'] : ['error'],
-});
-
-module.exports = prisma;
